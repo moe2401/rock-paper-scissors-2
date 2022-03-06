8d57097197a47a0818eb23dc510a6b7c8e874d54
moe <moe.aoki0124@gmail.com>
2022-03-06 12:52:41 +1000

    finished html and css, started javascript.

---
+++ main.js
@@ -0,0 +1,6 @@
+//DOM variables
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
