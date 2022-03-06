2ed0fa8cb8df69fc58817ba7e21a18a8a12a5f76
moe <moe.aoki0124@gmail.com>
2022-03-06 20:59:02 +1000

    finished main functionality of javascript.

--- main.js
+++ main.js
@@ -1,26 +1,72 @@
 //DOM variables
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-
+//randomized computer choice
 function getComputerChoice(){
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
+function letterToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function userWin(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = " (user)".fontsize(3).sup();
+    const smallCompWord = " (comp)".fontsize(3).sup();
+    result_p.innerHTML = `${letterToWord(userChoice)}${smallUserWord} beats ${letterToWord(computerChoice)}${smallCompWord}. User wins!`;
+}
+
+function computerWin(userChoice, computerChoice){
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    userScore_span.innerHTML = userScore;
+    const smallUserWord = " (user)".fontsize(3).sup();
+    const smallCompWord = " (comp)".fontsize(3).sup();
+    result_p.innerHTML = `${letterToWord(computerChoice)}${smallCompWord} beats ${letterToWord(userChoice)}${smallUserWord}. Computer wins!`;
+}
+
+function draw(userChoice, computerChoice){
+    const smallUserWord = " (user)".fontsize(3).sup();
+    const smallCompWord = " (comp)".fontsize(3).sup();
+    result_p.innerHTML = `${letterToWord(computerChoice)}${smallCompWord} equals ${letterToWord(userChoice)}${smallUserWord}. It is a draw!`;
+}
+
 
 function game(userChoice){
     const computerChoice = getComputerChoice();
-    console.log("User choice is " + userChoice + " !");
-    console.log("Computer choice is " + computerChoice + " !");
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            userWin(userChoice, computerChoice);
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            computerWin(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+        
+    }
 }
 
 
